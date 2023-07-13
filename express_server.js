24b457cb8b2da913f03a60df874887cc1f4da2a2
Shajee-j <Shajeejm@gmail.com>
2023-07-13 10:21:48 -0400

    implement post.request for forum

--- express_server.js
+++ express_server.js
@@ -2,13 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs");
 
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -29,6 +32,11 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); 
+  res.send("Ok"); 
 });
 
 app.get("/urls/new", (req, res) => {
